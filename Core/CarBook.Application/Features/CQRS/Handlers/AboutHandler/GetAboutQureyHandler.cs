4dd0b9e8e7e2d86b8e11e5d22528dc4b0f51ed6d
eakpinarr <eakpinarfizikci@gmail.com>
2023-12-24 13:06:03 +0300

    Add project files.

---
+++ Core/CarBook.Application/Features/CQRS/Handlers/AboutHandler/GetAboutQureyHandler.cs
@@ -0,0 +1,20 @@
+﻿using CarBook.Application.Interfaces;
+using CarBook.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarBook.Application.Features.CQRS.Handlers.AboutHandler
+{
+    public class GetAboutQureyHandler
+    {
+        private readonly IRepository<About> _repository;
+
+        public GetAboutQureyHandler(IRepository<About> repository)
+        {
+            _repository = repository;
+        }
+    }
+}
---
+++ Core/CarBook.Domain/Entities/Car.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarBook.Domain.Entities
+{
+    public class Car
+    {
+        [Key]
+        public int CarID { get; set; }
+        public int BrandID { get; set; }
+        public Brand Brand { get; set; }
+        public string Model { get; set; }
+        public string CoverImageURL { get; set; }
+        public int Km {  get; set; }
+        public string Transmission {  get; set; }
+        public byte seat {  get; set; }
+        public byte Luggage { get; set; }
+        public string Fuel { get; set; }
+        public string BigImageURL { get; set; }
+        public List<CarFeature> CarFeatures { get; set; }
+        public List<CarDescription> CarDescriptions { get; set; }
+        public List<CarPricing> CarPricings { get; set; }
+
+    }
+}
